ab93a92a2992c18ee5f748bcceefc2cbad0d2a91
Petr Blaho <petrblaho@gmail.com>
2026-07-15 16:14:30 +0200

    test(bin): add integration test verifying silence of git warning during boot
    Created with the use of gemini-3.5-flash via Norn

--- spec/norn/bin_norn_spec.rb
+++ spec/norn/bin_norn_spec.rb
@@ -13,4 +13,30 @@ RSpec.describe "bin/norn executable" do
     expect(stderr).to include("norn chat")
     expect(stderr).to include("Start an interactive chat session with Norn")
   end
+
+  it "silences git stderr warnings when booting outside of a git repository" do
+    require "tmpdir"
+    require "fileutils"
+
+    Dir.mktmpdir do |temp_dir|
+      # Copy minimum required boot assets to the temp directory
+      FileUtils.cp_r(File.expand_path("../../bin", __dir__), temp_dir)
+      FileUtils.cp_r(File.expand_path("../../lib", __dir__), temp_dir)
+      FileUtils.cp_r(File.expand_path("../../plugins", __dir__), temp_dir)
+      FileUtils.cp(File.expand_path("../../Gemfile", __dir__), temp_dir)
+      FileUtils.cp(File.expand_path("../../Gemfile.lock", __dir__), temp_dir)
+      FileUtils.cp(File.expand_path("../../norn.gemspec", __dir__), temp_dir)
+
+      # Run the executable inside the temp directory (which lacks any parent .git)
+      temp_bin_path = File.join(temp_dir, "bin", "norn")
+      
+      _stdout, stderr, _status = Bundler.with_unbundled_env do
+        Open3.capture3(temp_bin_path)
+      end
+
+      # Assert that git complains are completely silenced
+      expect(stderr).not_to include("fatal: not a git repository")
+      expect(stderr).not_to include("Stopping at filesystem boundary")
+    end
+  end
 end
